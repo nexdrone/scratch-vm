36dafb1e3396200e617016b6c64840084a41e426
tetsuya tagashira <t.tagashira@nexdrone.jp>
2021-06-19 02:35:27 +0000

    2021/06/19 変更

--- src/extensions/scratch3_udltello/index.js
+++ src/extensions/scratch3_udltello/index.js
@@ -20,6 +20,26 @@ const menuIconURI = 'data:image/svg+xml;base64,PHN2ZyB2ZXJzaW9uPSIxLjEiIHdpZHRoP
 const telloNotConnectMessage = 'Telloに接続できません。\n';
 
 /**
+ * 計算単位
+ *   1: 時間
+ *   2: 距離
+ */
+const calc_unit = 1;
+
+/**
+ * コマンドの名称一覧
+ */
+const upDesc = ['秒 上昇する', 'cm 上昇する'];
+const downDesc = ['秒 下降する', 'cm 下降する'];
+const leftDesc = ['秒 左に移動する', 'cm 左に移動する'];
+const rightDesc = ['秒 右に移動する', 'cm 右に移動する'];
+const forwardDesc = ['秒 前進する', 'cm 前進する'];
+const backwardDesc = ['秒 後退する', 'cm 後退する'];
+const leftturnDesc = ['秒 左旋回する', '度 左旋回する'];
+const rightturnDesc = ['秒 右旋回する', '度 右旋回する'];
+
+
+/**
  * Class for the new blocks in Scratch 3.0
  * @param {Runtime} runtime - the runtime instantiating this block package.
  * @constructor
@@ -48,18 +68,106 @@ class Scratch3UdlTello {
             blockIconURI: blockIconURI,
             blocks: [
                 {
-                    opcode: 'initialize',
-                    text: 'initialize',
-                    blockType: BlockType.COMMAND
-                },
-                {
                     opcode: 'takeoff',
-                    text: 'takeoff',
+                    text: '離陸する',
                     blockType: BlockType.COMMAND
                 },
                 {
                     opcode: 'land',
-                    text: 'land',
+                    text: '着陸する',
+                    blockType: BlockType.COMMAND
+                },
+                {
+                    opcode: 'up',
+                    text: '[x] ' + upDesc[calc_unit - 1],
+                    blockType: BlockType.COMMAND,
+                    arguments: {
+                        x: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 1
+                        }
+                    }
+                },
+                {
+                    opcode: 'down',
+                    text: '[x] ' + downDesc[calc_unit - 1],
+                    blockType: BlockType.COMMAND,
+                    arguments: {
+                        x: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 1
+                        }
+                    }
+                },
+                {
+                    opcode: 'leftmove',
+                    text: '[x] ' + leftDesc[calc_unit - 1],
+                    blockType: BlockType.COMMAND,
+                    arguments: {
+                        x: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 1
+                        }
+                    }
+                },
+                {
+                    opcode: 'rightmove',
+                    text: '[x] ' + rightDesc[calc_unit - 1],
+                    blockType: BlockType.COMMAND,
+                    arguments: {
+                        x: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 1
+                        }
+                    }
+                },
+                {
+                    opcode: 'forward',
+                    text: '[x] ' + forwardDesc[calc_unit - 1],
+                    blockType: BlockType.COMMAND,
+                    arguments: {
+                        x: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 1
+                        }
+                    }
+                },
+                {
+                    opcode: 'backward',
+                    text: '[x] ' + backwardDesc[calc_unit - 1],
+                    blockType: BlockType.COMMAND,
+                    arguments: {
+                        x: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 1
+                        }
+                    }
+                },
+                {
+                    opcode: 'leftturn',
+                    text: '[x] ' + leftturnDesc[calc_unit - 1],
+                    blockType: BlockType.COMMAND,
+                    arguments: {
+                        x: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 90
+                        }
+                    }
+                },
+                {
+                    opcode: 'rightturn',
+                    text: '[x] ' + rightturnDesc[calc_unit - 1],
+                    blockType: BlockType.COMMAND,
+                    arguments: {
+                        x: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 90
+                        }
+                    }
+                },
+                {
+                    opcode: 'test',
+                    text: 'テスト',
                     blockType: BlockType.COMMAND
                 },
             ],
@@ -71,19 +179,6 @@ class Scratch3UdlTello {
     /**
      * Common Methods
      */
-    
-    /**
-     * window.csEvent が定義されているかどうか確認する
-     */
-    _checkCsEvent () {
-        if (!window.csEvent) {
-            alert('window.csEvent does not exist.');
-            return false;
-        }
-        return true;
-    }
-
-
     /**
      * Write log.
      * @param {object} args - the block arguments.
@@ -95,32 +190,17 @@ class Scratch3UdlTello {
     }
 
     /**
-     * Get the browser.
-     * @return {number} - the user agent.
-     */
-    getBrowser () {
-        //return navigator.userAgent;
-        if (!window.csEvent) {
-            alert('window.csEvent does not exist.');
-            return;
-        }
-        window.csEvent.raiseEvent('click', JSON.stringify({
-            data1: 'memo',
-        }));
-    }
-
-    /**
      * 離陸
      * @return {number} - the user agent.
      */
     takeoff () {
-        CefSharp.BindObjectAsync("boundAsync").then(function(result)
+        CefSharp.BindObjectAsync("boundAsync").then((result) =>
         {
-            boundAsync.takeoff().then(function (result)
+            boundAsync.takeoff().then((result) =>
             {
                 const obj = JSON.parse(result);
-                if (!result.status) {
-                    alert(telloNotConnectMessage);
+                if (!obj.status) {
+                    alert(obj.message);
                 }
             });
         });
@@ -131,19 +211,176 @@ class Scratch3UdlTello {
      * @return {number} - the user agent.
      */
     land () {
-        //if (!this._checkCsEvent()){ return; }
-        //window.csEvent.raiseEvent('land', JSON.stringify({
-        //}));
-        CefSharp.BindObjectAsync("boundAsync").then(function(result)
+        CefSharp.BindObjectAsync("boundAsync").then((result) =>
         {
-            boundAsync.land().then(function (result)
+            boundAsync.land().then((result) =>
             {
                 const obj = JSON.parse(result);
-                if (!result.status) {
-                    alert(telloNotConnectMessage);
+                if (!obj.status) {
+                    alert(obj.message);
                 }
             });
         });
+    }
+
+    /**
+     * 上昇
+     */
+    up (args) {
+        const x = Cast.toNumber(args.x);
+        CefSharp.BindObjectAsync("boundAsync").then((result) =>
+        {
+            boundAsync.up(x, calc_unit).then((result) =>
+            {
+                const obj = JSON.parse(result);
+                if (!obj.status) {
+                    alert(obj.message);
+                }
+            });
+        });
+    }
+
+    /**
+     * 下降
+     */
+    down (args) {
+        const x = Cast.toNumber(args.x);
+        CefSharp.BindObjectAsync("boundAsync").then((result) =>
+        {
+            boundAsync.down(x, calc_unit).then((result) =>
+            {
+                const obj = JSON.parse(result);
+                if (!obj.status) {
+                    alert(obj.message);
+                }
+            });
+        });
+    }
+
+    /**
+     * 左移動
+     */
+    leftmove (args) {
+        const x = Cast.toNumber(args.x);
+        CefSharp.BindObjectAsync("boundAsync").then((result) =>
+        {
+            boundAsync.left(x, calc_unit).then((result) =>
+            {
+                const obj = JSON.parse(result);
+                if (!obj.status) {
+                    alert(obj.message);
+                }
+            });
+        });
+    }
+
+    /**
+     * 右移動
+     */
+    rightmove (args) {
+        const x = Cast.toNumber(args.x);
+        CefSharp.BindObjectAsync("boundAsync").then((result) =>
+        {
+            boundAsync.right(x, calc_unit).then((result) =>
+            {
+                const obj = JSON.parse(result);
+                if (!obj.status) {
+                    alert(obj.message);
+                }
+            });
+        });
+    }
+
+    /**
+     * 前進
+     */
+    forward (args) {
+        const x = Cast.toNumber(args.x);
+        CefSharp.BindObjectAsync("boundAsync").then((result) =>
+        {
+            boundAsync.forward(x, calc_unit).then((result) =>
+            {
+                const obj = JSON.parse(result);
+                if (!obj.status) {
+                    alert(obj.message);
+                }
+            });
+        });
+    }
+
+    /**
+     * 後進
+     */
+    backward (args) {
+        const x = Cast.toNumber(args.x);
+        CefSharp.BindObjectAsync("boundAsync").then((result) =>
+        {
+            boundAsync.backward(x, calc_unit).then((result) =>
+            {
+                const obj = JSON.parse(result);
+                if (!obj.status) {
+                    alert(obj.message);
+                }
+            });
+        });
+    }
+
+    /**
+     * 左旋回
+     */
+    leftturn (args) {
+        const x = Cast.toNumber(args.x);
+        CefSharp.BindObjectAsync("boundAsync").then((result) =>
+        {
+            boundAsync.leftturn(x, calc_unit).then((result) =>
+            {
+                const obj = JSON.parse(result);
+                if (!obj.status) {
+                    alert(obj.message);
+                }
+            });
+        });
+    }
+
+    /**
+     * 右旋回
+     */
+    rightturn (args) {
+        const x = Cast.toNumber(args.x);
+        CefSharp.BindObjectAsync("boundAsync").then((result) =>
+        {
+            boundAsync.rightturn(x, calc_unit).then((result) =>
+            {
+                const obj = JSON.parse(result);
+                if (!obj.status) {
+                    alert(obj.message);
+                }
+            });
+        });
+    }
+
+    test () {
+        // この呼び出しはOK
+        //this._alert("test");
+
+        CefSharp.BindObjectAsync("boundAsync").then((result) =>
+        {
+            boundAsync.rightturn(1, calc_unit).then((result) =>
+            {
+                const obj = JSON.parse(result);
+                if (!obj.status) {
+                    this._alert(obj.message);
+                    //alert(obj.message);
+                }
+            });
+        });
+
+    }
+
+
+
+    _alert(x) {
+        alert(x);
     }
 }
 
